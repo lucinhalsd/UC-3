a5b339a52d28b0d3306865dc70d03336d9498b9e
LUCIMARA <lucinha.lsd06@gmail.com>
2024-12-10 21:05:27 -0300

    aula10 playground

--- projetos aecio/script.js
+++ projetos aecio/script.js
@@ -3,64 +3,54 @@ const addBtn = document.getElementById("addBtn");
 const listaDeTarefas = document.getElementById("listaDeTarefas");
 
 addBtn.addEventListener("click", () => {
-    let valordoinput = tarefainput.value;
-    if(valorDoInput) {
-      criarTarefa(valorDoInput);
-      tarefaInput.value = "";
-      tarefaInput.focus();
-  } else {
-      alert("Digite uma tarefa válida!");
-      tarefaInput.focus();
-  }
+    let valorDoInput = tarefaInput.value;
+    if (valorDoInput) {
+        criarTarefa(valorDoInput);
+        tarefaInput.value = "";
+        tarefaInput.focus();
+    } else {
+        alert("Digite uma tarefa válida!");
+        tarefaInput.focus();
+    }
 })
 
-function criartarefa(Titulo) {
- const li = document.createElement("li")
+function criarTarefa(titulo) {
+    const li = document.createElement("li");
 
-  // Adicionar texto a uma tarefa
- const textodatarefa = document.createElement("spam");
- textodatarefa.innertext = titulo
+    // Adicionar texto a uma tarefa
+    const textoDaTarefa = document.createElement("span");
+    textoDaTarefa.innerText = titulo;
 
- // Adicionar o checkbox
- const checkbox = document.createElement("input");
- checkbox.type = "checkbox";
+    // Adicionar o Checkbox
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
 
- checkbox.addEventListener("change", ( => {
-    // li.classList.toggle("completed", checkbox.checked)
+    checkbox.addEventListener("change", () => {
 
-        // if(checkbox.checked) {
-        //     li.className = "completed";
-        // } else {
-        //     li.className = "";
-        // }
+        if (checkbox.checked) {
+            li.classList.add("completed");
+        } else {
+            li.classList.remove("completed");
+        }
+    })
 
-        
-    if(checkbox.checked) {
-     li.classList.add("completed");
-   }else {
-    li.classList.remove("completed");
-   }
-})
+    const removeBtn = document.createElement("button");
+    removeBtn.innerHTML = "<strong>🗑</strong>"
+    removeBtn.addEventListener("click", () => {
+        listaDeTarefas.removeChild(li);
+    });
 
- const removebtn = document.createElement("button");
- removeBtn.innerHTML = "<strong>🗑</strong>"
- removebtn.addEventListener("click", () => {
-   listaDeTarefas.removeChild(li)
- });
+    const divFlexLi = document.createElement("div");
+    divFlexLi.append(checkbox);
+    divFlexLi.append(textoDaTarefa);
 
- const divFlexLi = document.createElement("div");
- divFlexLi.append(checkbox);
- divFlexLi.append(textoDaTarefa);
-
-
- li.append(checkbox);
- li.append(textodatarefa) ;
- li.append(removebtn);
- listaDeTarefas.append(li);
+    li.append(divFlexLi);
+    li.append(removeBtn);
+    listaDeTarefas.append(li);
 }
 
 tarefaInput.addEventListener("keydown", (event) => {
-  if(event.key === "Enter") {
-    addBtn.click();
-  }
+    if(event.key === "Enter") {
+        addBtn.click();
+    }
 })

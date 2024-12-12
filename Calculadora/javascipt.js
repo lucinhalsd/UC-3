f716f70c893b98e802fd9e9ff85604303e7ebb79
LUCIMARA <lucinha.lsd06@gmail.com>
2024-12-11 21:32:53 -0300

    calculadora2

---
+++ Calculadora/javascipt.js
@@ -0,0 +1,18 @@
+//scipt.js
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try {
+        let result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Erro';
+    }
+}
